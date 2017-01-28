d717f965e401e6725a373d6050c4b4fce1e295c9
Alex Burner <alex.burner@gmail.com>
2017-01-28 13:45:18 -0800

    frist

---
+++ count.js
@@ -0,0 +1,24 @@
+let fs = require('fs');
+
+let file = __dirname + '/article.txt';
+
+fs.readFile(file, 'utf8', (err, data) => {
+    console.log('readFile:', file);
+    if (err) return console.error(err);
+    if (!data) return console.error('no data');
+    let words = data.split(/[^\w|’]|\d/);
+    let wordMap = new Map();
+    words.forEach(word => {
+        if (!word || !word.length) return;
+        word = word.toLowerCase();
+        wordMap.set(word, true);
+    });
+    let totalCount = words.length;
+    let uniqueCount = wordMap.size;
+    let ratio = uniqueCount / totalCount;
+    console.log('total word count =', totalCount);
+    console.log('unique word count =', uniqueCount);
+    console.log('unique/total ratio =', ratio);
+    // console.log(words);
+    // console.log(wordMap);
+});
